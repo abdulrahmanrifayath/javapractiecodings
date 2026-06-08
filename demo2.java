be26ddbf63817f7700a5ee54aa5246971fe6a3b3
abdulrahmanrifayath <abdulrahmanrifayath264@gmail.com>
2026-06-08 11:19:07 +0530

    jun8

---
+++ demo2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class demo2 {
+    public static void main(String[] args) {
+        //find largest among 5 values
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a number:");
+        int a = sc.nextInt();
+        System.out.print("enter a number:");
+        int b = sc.nextInt();
+        System.out.print("enter a number:");
+        int c = sc.nextInt();
+        System.out.print("enter a number:");
+        int d = sc.nextInt();
+        System.out.print("enter a number:");
+        int e = sc.nextInt();
+        
+        
+    }
+}
