d7c7298ac987d55066c6679220f76f7fd02912f8
abdulrahmanrifayath <abdulrahmanrifayath264@gmail.com>
2026-06-08 12:08:36 +0530

    jun8practice

--- demo2.java
+++ demo2.java
@@ -13,7 +13,24 @@ public class demo2 {
         int d = sc.nextInt();
         System.out.print("enter a number:");
         int e = sc.nextInt();
-        
-        
+
+        if(a>b && a>c && a>d && a>e){
+            System.out.println(a);
+        }
+        else if(b>a && b>c && b>d && b>e)
+        {
+            System.out.println(b);
+        }
+        else if(c>a && c>b && c>d && c>e)
+        {
+            System.out.println(c);
+        }
+        else if(d>a && d>b && d>c && d>e)
+        {
+            System.out.println(d);
+    
+        }
+        else
+        {System.err.println(e);}
     }
 }
