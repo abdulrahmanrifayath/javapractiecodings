4d658aacc673e1513a21eead594223129f00c124
abdulrahmanrifayath <abdulrahmanrifayath264@gmail.com>
2026-06-09 10:47:04 +0530

    jun9

--- demo5.java
+++ demo5.java
@@ -1,4 +1,13 @@
+import java.util.*;
 public class demo5 {
     public static void main(String[] args) {
-        
+        System.out.println("Enter the value:");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int factorial =1;
+        for(int i=1; i<=num; i++){
+            factorial = factorial * i;
+        }
+        System.out.println("Factorial="+factorial);
+    }
 }
