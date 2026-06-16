cdaf672533a596abb981528422626f2421faf98d
abdulrahmanrifayath <abdulrahmanrifayath264@gmail.com>
2026-06-16 16:21:56 +0530

    chages

--- demo15.java
+++ demo15.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class switchfunction {
+public class demo15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Select an option:");
