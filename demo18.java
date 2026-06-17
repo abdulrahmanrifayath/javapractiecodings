9fe856039170d12a3a494d29f3941fbae0ea9e2a
abdulrahmanrifayath <abdulrahmanrifayath264@gmail.com>
2026-06-17 16:38:21 +0530

    jun17

--- demo18.java
+++ demo18.java
@@ -2,12 +2,20 @@ import java.util.*;
 public class demo18{
     public static void main(String[] args) {
         System.out.print("Enter the text:");
-
         Scanner sc = new Scanner(System.in);
-        String value = sc.nextLine();
-        for (int i=0; i<value.length(); i++){
-            char ch = value.charAt(i);
-            
+        String s = sc.nextLine();
+        int n = s.length();
+        for (int i=0; i<s.length(); i++){
+            boolean flag = true;
+            for(int j =0; j<n; j++){
+                if(i != j && s.charAt(i) == s.charAt(j)){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                System.out.println(s.charAt(i));
+            }
         }
     }
 }
