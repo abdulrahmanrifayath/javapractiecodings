855d35079fce5c1ec56c755ff51382dde503f591
abdulrahmanrifayath <abdulrahmanrifayath264@gmail.com>
2026-06-17 16:04:43 +0530

    jun17

--- demo18.java
+++ demo18.java
@@ -1,5 +1,10 @@
+import java.util.*;
 public class demo18{
     public static void main(String[] args) {
+        System.out.print("Enter the text:");
+
+        Scanner sc = new Scanner(System.in);
+        String value = sc.nextLine();
         
     }
 }
