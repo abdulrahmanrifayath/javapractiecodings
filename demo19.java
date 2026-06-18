e3fdd830287a6035be66538595cfbb27925e5e57
abdulrahmanrifayath <abdulrahmanrifayath264@gmail.com>
2026-06-18 10:41:34 +0530

    jun18

---
+++ demo19.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class demo19{
+    public static void main(String[] args){
+        System.out.print("enter the value:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<=n; i++){
+            for(int j= 1;j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
